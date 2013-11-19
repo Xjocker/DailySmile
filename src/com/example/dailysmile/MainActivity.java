6d08b08712d68c3c30b1cd7cc69fc6dc9d0340c1
Xjocker <rigo008@live.fr>
2013-11-19 03:18:23 +0100

    commit 19/11/13  (1st and 2nd activities ready)

--- src/com/example/dailysmile/MainActivity.java
+++ src/com/example/dailysmile/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.dailysmile;
 
+
+import android.view.View;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.view.Menu;
+import android.content.Intent;
+
 
 
 
@@ -12,6 +17,26 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+	    //Start the second activity GetTimeActivity after 5 seconds
+		int DELAY = 5000;
+
+	    Handler handler = new Handler();
+	    handler.postDelayed(new Runnable() {            
+	        @Override
+	        public void run() {
+	        	
+	        	//Start the second activity GetTimeActivity
+	            Intent intent = new Intent(MainActivity.this, GetTimeActivity.class);
+	            startActivity(intent);  
+	            finish(); //To exit the app if we click on back button in the second activity so it finishes the fist activity directly
+	        }
+	    }, DELAY);
+
+
+	    
+	    
+		
 	}
 
 	@Override
